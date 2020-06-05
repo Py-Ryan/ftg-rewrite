3f2b06c2f35f61d60e735b82f905caed67084dbf
Py-Ryan <pyryanggg@gmail.com>
2020-06-04 21:04:52 -0400

    Dynamic prefixing Built-in aiohttp.ClientSession() Ftg.extensions yields None if no extensions are passed to the constructor Setup DB parsing for guild specific data such as custom prefixes. Ftg now has a cache Added a new cog `config`

--- README.md
+++ README.md
@@ -2,7 +2,25 @@
 A rewrote version of Ft.-Gunna
 
 # version:
-0.1.1
+0.1.0
 
 # updates:
-    - Remove unneeded kwargs pop in bot constructor. 
+    0.1.0:
+        Barebones version.
+    0.2.0:
+        - Dynamic prefixing
+        - Built-in aiohttp.ClientSession() since aiohttp suggests this
+        - Implemented Jishaku
+        - Ftg.extensions yields None if no extensions are passed to the constructor
+        - Setup DB parsing for guild specific data such as custom prefixes.
+        - Ftg now has a cache
+            ->
+                - This cache is used to store
+                    ->
+                        - Prefixes for a guild during init and during runtime.
+                        - Deleted & edited messages. 
+        - Added a new cog `config`
+            ->
+                - Features commands relating to guild & user configuration
+                    ->
+                        - Added a change prefix command
--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,12 @@
 import toml
 
 from os import listdir, path
+from collections import deque
 from datetime import datetime
 from asyncpg import create_pool
 from discord.ext import commands
+from aiohttp import ClientSession
+
 
 with open('config.toml', 'r') as file:
     config = toml.load(file)
@@ -11,17 +14,17 @@ with open('config.toml', 'r') as file:
 
 
 class Ftg(commands.Bot):
-    __slots__ = ("_extensions", "_db_url", "_token", "_raw_uptime", "db", "cache")
+    __slots__ = ("_extensions", "_db_url", "_token", "_raw_uptime", "db", "cache", "session")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._extensions = kwargs.pop('extensions', None)
+        self._extensions = kwargs.pop('extensions', [])
         self._db_url = kwargs.pop('db_url', None)
         self._token = kwargs.pop('token', None)
         self._raw_uptime = datetime.utcnow()
+        self.session = None
         self.cache = {}
-        self.run()
-        self.db = self.loop.run_until_complete(create_pool(self._db_url))
+        self.db = None
 
     @property
     def uptime(self):
@@ -31,8 +34,15 @@ class Ftg(commands.Bot):
     @property
     def extensions(self):
         """Generator yielding the a (filename, extension) tuple of the extensions."""
-        for cog in self._extensions:
-            yield path.splitext(cog)
+        if not self._extensions:
+            yield None
+        else:
+            for cog in self._extensions:
+                yield path.splitext(cog)
+
+    async def setup(self):
+        self.db = await create_pool(self._db_url, min_size=1, max_size=5)
+        self.session = ClientSession()
 
     def run(self):
         """Connects the bot to discord & mounts the cogs."""
@@ -40,8 +50,23 @@ class Ftg(commands.Bot):
             if ext == '.py' and not base.startswith('__'):
                 self.load_extension(f'cogs.{base}')
                 print(f'Loaded {base}.')
-        self.load_extension("jishaku")
 
+        self.loop.run_until_complete(self.setup())
+
+        guilds = self.loop.run_until_complete(
+            self.db.fetch(
+                """
+                SELECT (id, prefix)
+                FROM guilds
+                """
+            )
+        )
+
+        for row in guilds:
+            (guild_id, prefix) = row['row']
+            self.cache[str(guild_id)] = {'prefix': prefix, 'messages': {'deleted': deque(), 'edited': deque()}}
+
+        self.load_extension("jishaku")
         super().run(self._token)
 
     def add_cog(self, cog):
@@ -53,4 +78,19 @@ class Ftg(commands.Bot):
             print(f'{self.user} is ready.')
 
 
-Ftg(token=config.get('token', None), extensions=listdir('../cogs'), command_prefix='gn ')
+def get_prefix(bot, message):
+    guild = bot.cache.setdefault(
+        str(message.guild.id),
+        {'prefix': 'gn ', 'messages': {'deleted': deque(), 'edited': deque()}}
+    )
+
+    return commands.when_mentioned_or(guild["prefix"])(bot, message)
+
+
+ftg = Ftg(
+    token=config.get('token', None),
+    extensions=listdir('../cogs'),
+    command_prefix=get_prefix,
+    db_url=config.get('db_url', None)
+)
+ftg.run()
