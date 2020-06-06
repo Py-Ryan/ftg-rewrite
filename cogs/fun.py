901f61f4d511d87a44f0f8afd46dd554c2b4a71d
Py-Ryan <pyryanggg@gmail.com>
2020-06-05 20:00:11 -0400

    Use a regex check to detect whether the entire input is binary or not. Better conversion detection.
    Edited the maximum message output length to 150 characters in the `binary` command.
    
    Changed `caesar` command decorator from group back to a command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,3 +1,4 @@
+import re
 from random import randint
 from discord.ext import commands
 from discord import Embed, AllowedMentions
@@ -14,22 +15,24 @@ class FunCog(commands.Cog):
     async def binary(self, ctx, *, text):
         """Convert text to binary or vise versa. Enter binary bytes separated by spaces to convert into ASCII."""
         try:
-            output = ''.join([chr(int(byte, 2)) for byte in text.split()])
+            binary = text.split()
+            if all(re.match(r'^[0-1]{8}$', b) for b in binary):
+                output = ''.join([chr(int(byte, 2)) for byte in binary])
+            else:
+                raise ValueError
         except ValueError:
             output = ' '.join([bin(ord(char))[2:].zfill(8) for char in text])
 
-        if len(output) >= 100:
-            async with self.bot.session.post('https://hastebin.com/documents', data=output) as post:
-                data = await post.json()
-
-                url_code = data.get('key', None)
+        if len(output) >= 150:
+            async with self.bot.session.post('https://haste.crrapi.xyz/documents', data=output) as post:
+                url_code = (await post.json()).get('key', None)
                 if url_code:
-                    await ctx.send(f'{ctx.author.mention}, https://hastebin.com/raw/{url_code}')
+                    await ctx.send(f'{ctx.author.mention}, https://haste.crrapi.xyz/raw/{url_code}')
         else:
             await ctx.send(f'{ctx.author.mention}, {output}',
                            allowed_mentions=AllowedMentions(everyone=False, roles=False))
 
-    @commands.group(invoke_without_command=True)
+    @commands.command()
     @commands.cooldown(1, 1.5, commands.BucketType.guild)
     async def caesar(self, ctx, *, text):
         """Convert plain text into a caesar cipher. Default shift factor is 4."""
