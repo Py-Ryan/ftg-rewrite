8a1b2e45f992e67b284f552579b28390fa55b23a
Py-Ryan <pyryanggg@gmail.com>
2020-06-09 20:06:20 -0400

    see changelog

--- bot/bot.py
+++ bot/bot.py
@@ -1,13 +1,13 @@
 import toml
-import asyncio
+
+from os import listdir
 from discord import Game
-from os import listdir, path
-from collections import deque
 from datetime import datetime
 from asyncpg import create_pool
 from discord.ext import commands
 from humanize import naturaldelta
 from aiohttp import ClientSession
+
 
 with open('config.toml', 'r') as file:
     config = toml.load(file).get('credentials', None)
@@ -27,48 +27,35 @@ class Context(commands.Context):
 
 
 class Ftg(commands.Bot):
-    __slots__ = ("_extensions", "_db_url", "_token", "_raw_uptime", "db", "cache", "session")
+    __slots__ = ("db_url", "token", "_raw_uptime", "db", "cache", "session")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._extensions = kwargs.pop('extensions', [])
-        self.api = {'ip': kwargs.pop('ip_key', None)}
-        self._db_url = kwargs.pop('db_url', None)
-        self._token = kwargs.pop('token', None)
-        self._raw_uptime = datetime.now()
-        self.session = None
         self.cache = {}
-        self.db = None
+        self._raw_uptime = datetime.now()
+        (self.db, self.session) = (None, None)
+
+        for (attribute, value) in kwargs.items():
+            setattr(self, attribute, value)
 
     @property
     def uptime(self):
         """Formatted bot uptime."""
         return naturaldelta(self._raw_uptime) if self._raw_uptime else None
 
-    @property
-    def extensions(self):
-        """Generator yielding the a (filename, extension) tuple of the extensions."""
-        if not self._extensions or not isinstance(self._extensions, (tuple, list, set)):
-            raise RuntimeWarning(
-                f"Invalid type passed to {type(self).__name__}._extensions. Must be one of tuple, list, or set. "
-                "Extensions will not be loaded")
-        else:
-            for cog in self._extensions:
-                yield path.splitext(cog)
-
     async def setup(self):
         (self.db, self.session) = (await create_pool(
-            self._db_url,
+            self.db_url,
             min_size=1,
             max_size=5,
         ), ClientSession())
 
     def run(self):
         """Connects the bot to discord & mounts the cogs."""
-        for base, ext in self.extensions:
-            if ext == '.py' and not base.startswith('__'):
-                self.load_extension(f'cogs.{base}')
-                print(f'Loaded {base}.')
+        for ext in self.modules:
+            if not ext.startswith('__'):
+                self.load_extension(f'cogs.{ext[:-3]}')
+                print(f'Loaded {ext}.')
 
         self.loop.run_until_complete(self.setup())
 
@@ -83,7 +70,7 @@ class Ftg(commands.Bot):
 
         for row in guilds:
             (guild_id, prefix) = row['row']
-            self.cache[str(guild_id)] = {'prefix': prefix, 'messages': {'deleted': deque(), 'edited': deque()}}
+            self.cache[str(guild_id)] = {'prefix': prefix}
 
         for (name, obj) in self.cogs.items():
             with open(f'../cogs/{name}.py', encoding='utf8') as extension:
@@ -92,7 +79,7 @@ class Ftg(commands.Bot):
                 obj._raw_uptime = datetime.now()
 
         self.load_extension("jishaku")
-        super().run(self._token)
+        super().run(self.token)
 
     async def on_ready(self):
         if self._raw_uptime <= datetime.utcnow():
@@ -112,10 +99,7 @@ class Ftg(commands.Bot):
 
 def get_prefix(bot, message):
     if message.guild is not None:
-        guild = bot.cache.setdefault(
-            str(message.guild.id),
-            {'prefix': 'gn ', 'messages': {'deleted': deque(), 'edited': deque()}}
-        )
+        guild = bot.cache.setdefault(str(message.guild.id), {'prefix': 'gn '})
         return commands.when_mentioned_or(guild["prefix"])(bot, message)
     else:
         return commands.when_mentioned_or('gn ')(bot, message)
@@ -123,7 +107,7 @@ def get_prefix(bot, message):
 
 ftg = Ftg(
     **config,
-    extensions=listdir('../cogs'),
+    modules=listdir('../cogs'),
     command_prefix=get_prefix,
     activity=Game('default prefix is a mention')
 )
