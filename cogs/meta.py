d5c7ae91875b2476e5f558abb8d8a3ad5b2b8367
Py-Ryan <pyryanggg@gmail.com>
2020-06-17 01:07:26 -0400

    Add a BetterUserConverter Good general improvements to avatar & info command.

--- cogs/meta.py
+++ cogs/meta.py
@@ -6,6 +6,21 @@ from typing import Union, Optional
 from discord import User, Embed, Member, Status, Colour, HTTPException
 
 
+class BetterUserConverter(commands.Converter):
+    """A user converter that supports fetching users out of the global user cache."""
+
+    async def convert(self, ctx, argument):
+        result = None
+        try:
+            converter = commands.UserConverter()
+            result = await converter.convert(ctx, argument)
+        except commands.BadArgument:
+            with suppress(HTTPException, TypeError):
+                result = await ctx.bot.fetch_user(int(argument))
+        finally:
+            return result
+
+
 class Meta(commands.Cog):
     """Cog used for general commands that don't belong anywhere specific."""
 
@@ -15,15 +30,8 @@ class Meta(commands.Cog):
     @commands.group(invoke_without_command=True)
     @commands.guild_only()
     @commands.cooldown(1, 1.5, commands.BucketType.guild)
-    async def info(self, ctx, *, snwflk: Union[Member, int, str] = None):
-        if snwflk == 'me':
-            snwflk = ctx.author
-        else:
-            if not isinstance(snwflk, Member):
-                try:
-                    snwflk = await self.bot.fetch_user(snwflk)
-                except HTTPException:
-                    snwflk = ctx.guild.me
+    async def info(self, ctx, *, snwflk: Union[Member, BetterUserConverter, str] = None):
+        snwflk = ctx.author if snwflk == 'me' else snwflk or ctx.guild.me
 
         cases = {
             Status.online: Colour.green(),
@@ -32,19 +40,22 @@ class Meta(commands.Cog):
             Status.offline: Colour.light_grey()
         }
 
+        git      = f'[Source Code](https://github.com/Py-Ryan/ftg-rewrite)'
+        uid      = snwflk.id
+        desc     =  f'[Avatar Link]({snwflk.avatar_url})' if uid != self.bot.user.id else git
+        colour   = cases[getattr(snwflk, 'status', Status.offline)]
+        avatar   = str(snwflk.avatar_url_as(static_format='png'))
+        joined   = naturaltime(getattr(snwflk, 'joined_at', 'None'))
+        created  = naturaltime(snwflk.created_at)
+        top_role = str(getattr(snwflk, 'top_role', None))
+
         embed = (
-            Embed(
-                title=str(snwflk),
-                colour=cases[snwflk.status] if not isinstance(snwflk, User) else Colour.dark_grey(),
-                description=f'[Avatar Link]({snwflk.avatar_url})'
-                if not snwflk.id == self.bot.user.id else
-                f'[Source Code](https://github.com/Py-Ryan/ftg-rewrite)'
-            )
-            .add_field(name='**Account Created**', value=naturaltime(snwflk.created_at), inline=True)
-            .add_field(name='**Joined**', value=naturaltime(getattr(snwflk, 'joined_at', 'None')), inline=True)
-            .add_field(name='**User ID**', value=snwflk.id, inline=False)
-            .add_field(name='**Top Role**', value=str(getattr(snwflk, 'top_role', 'None')), inline=False)
-            .set_thumbnail(url=str(snwflk.avatar_url_as(static_format='png')))
+            Embed(title=str(snwflk), colour=colour, description=desc)
+            .add_field(name='**Account Created**', value=created, inline=True)
+            .add_field(name='**Joined**', value=joined, inline=True)
+            .add_field(name='**User ID**', value=uid, inline=False)
+            .add_field(name='**Top Role**', value=top_role, inline=False)
+            .set_thumbnail(url=avatar)
         )
 
         if isinstance(snwflk, User):
@@ -53,9 +64,12 @@ class Meta(commands.Cog):
                 embed.remove_field(i)
 
         if snwflk is ctx.guild.me:
+            uptime  = self.bot.uptime
+            latency = round(self.bot.latency * 1000)
             version = self.bot.__version__
-            embed.add_field(name='**Uptime**', value=self.bot.uptime)
-            embed.add_field(name='**Latency**', value=f'{round(self.bot.latency * 1000)}ms', inline=False)
+
+            embed.add_field(name='**Uptime**', value=uptime)
+            embed.add_field(name='**Latency**', value=f'{latency}ms', inline=False)
             embed.set_footer(text=f'Developer: well in that case#0082 (700091773695033505) | Version: {version}')
 
         await ctx.send(embed=embed)
@@ -66,6 +80,11 @@ class Meta(commands.Cog):
         cog = cog.capitalize()
         info_cog = self.bot.cogs.get(cog, None)
 
+        loc       = info_cog.loc
+        avatar    = str(self.bot.user.avatar_url_as(static_format='png'))
+        uptime    = naturaltime(info_cog._raw_uptime)
+        cmd_count = len(info_cog.get_commands())
+
         if info_cog:
             info = (
                 Embed(
@@ -73,10 +92,10 @@ class Meta(commands.Cog):
                     description=f'Information on the {cog.lower()} extension.',
                     colour=randint(0, 0xffffff)
                 )
-                .add_field(name='**Command Count**', value=len(info_cog.get_commands()), inline=True)
-                .add_field(name='**Lines Of Code**', value=info_cog.loc, inline=True)
-                .set_thumbnail(url=str(self.bot.user.avatar_url_as(static_format='png')))
-                .set_footer(text=f'Extension loaded {naturaltime(info_cog._raw_uptime)}.')
+                .add_field(name='**Command Count**', value=cmd_count, inline=True)
+                .add_field(name='**Lines Of Code**', value=loc, inline=True)
+                .set_thumbnail(url=avatar)
+                .set_footer(text=f'Extension loaded {uptime}.')
             )
 
             await ctx.send(embed=info)
@@ -85,19 +104,11 @@ class Meta(commands.Cog):
 
     @commands.command(aliases=['avatar', 'pfp'])
     @commands.cooldown(1, 1.5, commands.BucketType.guild)
-    async def av(self, ctx, *, user: Union[User, int, str] = None):
-        if user == 'me' or not user:
-            user = ctx.author
-        else:
-            if not isinstance(user, User):
-                try:
-                    user = await self.bot.fetch_user(user)
-                except HTTPException:
-                    user = ctx.author
-
+    async def av(self, ctx, *, user: Union[BetterUserConverter, str] = None):
+        user   = ctx.author if not user or user == 'me' else user
         avatar = str(user.avatar_url_as(static_format='png'))
 
-        embed = Embed(title=f"{user}'s avatar:", url=avatar)
+        embed = Embed(title=f"{user}'s avatar:", url=avatar, colour=randint(0, 0xffffff))
         embed.set_image(url=avatar)
 
         await ctx.send(embed=embed)
